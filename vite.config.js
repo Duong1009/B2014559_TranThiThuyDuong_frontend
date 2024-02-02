5a95644294b5ed0e7935b6f4992fdcda286db65c
Duong1009 <duongb2014559@student.ctu.edu.vn>
2024-02-02 10:30:38 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -15,11 +15,11 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-    proxy : {
+    proxy: {
       "/api": {
         target: "http://localhost:3000/",
         changeOrigin: true,
       },
     }
-  }
+  },
 })
